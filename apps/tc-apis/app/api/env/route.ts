4061824b441396cf03c4bc80d15e32df60597146
jscontreras <sebastian.contreras@vercel.com>
2025-03-11 09:47:04 -0500

    Listing SALUTE as part of envs.

--- apps/tc-apis/app/api/env/route.ts
+++ apps/tc-apis/app/api/env/route.ts
@@ -3,15 +3,22 @@ import { NextResponse } from "next/server";
 export async function GET() {
 
   const env_vars: any = {};
-  ['NEW_RELIC_APP_NAME', 'NEW_RELIC_LICENSE_KEY', 'WEBHOOK_SECRET', 'NEW_RELIC_USER_KEY'].forEach(key => {
+  const var_names = [
+    'NEW_RELIC_APP_NAME',
+    'NEW_RELIC_LICENSE_KEY',
+    'WEBHOOK_SECRET',
+    'NEW_RELIC_USER_KEY',
+    'SALUTE'
+  ];
+  var_names.forEach(key => {
     const value = process.env[key];
     if (value) {
       env_vars[key] = 'x'.repeat(value.length);
     }
-  })
+  });
   return NextResponse.json(
-        { env_vars },
-        { status: 200 },
+    { env_vars },
+    { status: 200 },
   );
 
 }
