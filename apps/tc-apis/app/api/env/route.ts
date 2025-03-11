572364eb5eef00bd8d6d43616d1e9003bcf637fd
jscontreras <sebastian.contreras@vercel.com>
2025-03-11 09:43:35 -0500

    Updating env vars.

---
+++ apps/tc-apis/app/api/env/route.ts
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+
+  const env_vars: any = {};
+  ['NEW_RELIC_APP_NAME', 'NEW_RELIC_LICENSE_KEY', 'WEBHOOK_SECRET', 'NEW_RELIC_USER_KEY'].forEach(key => {
+    const value = process.env[key];
+    if (value) {
+      env_vars[key] = 'x'.repeat(value.length);
+    }
+  })
+  return NextResponse.json(
+        { env_vars },
+        { status: 200 },
+  );
+
+}
