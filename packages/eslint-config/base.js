22be42c3b7ef4262bd4c29dd8a37e78a2138581a
jscontreras <sebastian.contreras@vercel.com>
2025-03-12 13:56:24 -0500

    Progresing.

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,6 +1,6 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import turboPlugin from "eslint-plugin-turbo";
+// import turboConfig from "eslint-config-turbo/flat";
 import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
 
@@ -10,17 +10,15 @@ import onlyWarn from "eslint-plugin-only-warn";
  * @type {import("eslint").Linter.Config[]}
  * */
 export const config = [
+  // ...turboConfig,
   js.configs.recommended,
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
-  {
-    plugins: {
-      turbo: turboPlugin,
-    },
-    rules: {
-      "turbo/no-undeclared-env-vars": "warn",
-    },
-  },
+  // {
+  //   rules: {
+  //     "turbo/no-undeclared-env-vars": "warn",
+  //   },
+  // },
   {
     plugins: {
       onlyWarn,
