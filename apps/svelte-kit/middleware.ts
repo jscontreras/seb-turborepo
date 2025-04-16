30b1849004929e9b7cfc9428e26af26c460dc574
jscontreras <sebastian.contreras@vercel.com>
2025-04-16 14:13:23 -0500

    Trying middleware.

---
+++ apps/svelte-kit/middleware.ts
@@ -0,0 +1,16 @@
+import { next } from '@vercel/edge';
+
+// config with custom matcher
+export const config = {
+  matcher: '/',
+};
+
+export default function middleware(request: Request) {
+  const geo = {
+    country: request.headers.get("x-vercel-ip-country") || null,
+    city: request.headers.get("x-vercel-ip-city") || null,
+    zipcode: request.headers.get("x-vercel-ip-postal-code") || null,
+  };
+  console.log('>>>>>MIDDLEWARE VERCEL', geo);
+  return next();
+}
