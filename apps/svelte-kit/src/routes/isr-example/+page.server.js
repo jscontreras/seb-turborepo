ce46e13e17a6c0a8623dd91aecad0f3e965499bb
jscontreras <sebastian.contreras@vercel.com>
2025-05-06 12:42:09 -0500

    Testing ISR.

--- apps/svelte-kit/src/routes/isr-example/+page.server.js
+++ apps/svelte-kit/src/routes/isr-example/+page.server.js
@@ -1,3 +1,12 @@
+import { BYPASS_TOKEN } from '$env/static/private';
+
+export const config = {
+  isr: {
+    expiration: 60,
+    bypassToken: BYPASS_TOKEN,
+    allowQuery: ['search']
+  }
+};
 export const load = async () => {
   const now = new Date().toISOString();
   return {
@@ -5,3 +14,4 @@ export const load = async () => {
     revalidate: 10
   };
 };
+
