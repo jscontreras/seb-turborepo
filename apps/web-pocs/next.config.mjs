2c11db771c1286988b33786f705880144fdcde88
jscontreras <sebastian.contreras@vercel.com>
2025-06-10 12:14:05 -0500

    Increasing ServerActions limit.

--- apps/web-pocs/next.config.mjs
+++ apps/web-pocs/next.config.mjs
@@ -3,7 +3,12 @@ const nextConfig = {
   transpilePackages: ["@repo/ui"],
   images: {
     remotePatterns: [new URL('https://xefbf5ydcrobj1vo.public.blob.vercel-storage.com/**'), new URL('https://oaidalleapiprodscus.blob.core.windows.net/**')]
-  }
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: '4mb',
+    },
+  },
 }
 
 export default nextConfig
